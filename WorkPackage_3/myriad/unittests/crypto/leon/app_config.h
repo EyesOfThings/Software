115cba8a96e0d673811638e901388381999c41c0
JavierParra <javier.parra@uclm.es>
2017-02-14 12:18:09 +0100

    Initial commit from private repository

---
+++ WorkPackage_3/myriad/unittests/crypto/leon/app_config.h
@@ -0,0 +1,16 @@
+#ifndef APP_CONFIG_H
+#define APP_CONFIG_H
+
+#include <mv_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int initClocksAndMemory(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
